5b083bf0f7c2c4659be8de621294e0526d0e57c5
Sean Kelly <github@katron.org>
2009-06-15 20:05:06 -0500

    Update config.fish
    Now with fewer annoying messages!

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,16 @@
-set PATH ~/local/bin $PATH
+set -x EDITOR vim
 
-# unset fish greeting
-set -e fish_greeting
+# clear out PATH first
+if status --is-login
+    set -l tmp_path
+    for path in $PATH;
+        if test -d $path;
+            set tmp_path $tmp_path $path
+        end
+    end
+    set PATH $tmp_path
+
+    set -l local_path $HOME/local/bin $HOME/local/games 
+
+    set PATH $local_path $PATH
+end
