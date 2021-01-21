7032be608f447c3b9c80e57c7d31f8b8482ec992
Sean Kelly <code@katron.org>
2021-01-21 11:13:28 -0500

    Merge in fish's default prompt in with my changes
    I like most of fish's prompt but I also like having two lines with the
    date in it. Copy most of fish's prompt then and modify it to what I'm
    used to.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -26,12 +26,27 @@ end
 
 ### Functions
 function fish_prompt
-    set last_status $status
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
-    printf ' %s' (date '+%Y-%m-%d %H:%M:%S')
-    printf '%s\n' (__fish_git_prompt)
-    printf '%s@%s> ' (whoami) (hostname)
-    #printf '> '
+    set -l last_pipestatus $pipestatus
+    set -l normal (set_color normal)
+
+    # Different prompt and cwd color for root.
+    set -l color_cwd $fish_color_cwd
+    set -l prompt '>'
+    if contains -- $USER root toor
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set prompt '#'
+    end
+
+    # Use different color hostname when running over SSH.
+    set -l color_host $fish_color_host
+    if set -q SSH_TTY
+        set color_host $fish_color_host_remote
+    end
+
+    set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+
+    echo -s (set_color $color_cwd) (prompt_pwd) $normal ' ' (date '+%Y-%m-%d %H:%M:%S') (fish_vcs_prompt)
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal $prompt_status $prompt " "
 end
